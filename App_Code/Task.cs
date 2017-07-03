09e9c49a21f8bb024249e9ec966777bc33485498
AdityaPrasadMishra <aditya.prasad.mishra.1989@gmail.com>
2017-07-02 18:33:18 -0700

    First Ciommit
    First Ciommit

---
+++ App_Code/Task.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Task
+/// </summary>
+public class Task
+
+{
+    public string name { get; set; }
+    public string startTime { get; set; }
+    public string finishTime { get; set; }
+    public Task()
+    {
+        
+    }
+}
---
+++ Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Script.Serialization;
+using System.Runtime.Serialization.Json;
+
+public partial class _Default : System.Web.UI.Page
+{
+    [System.Web.Services.WebMethod]
+    public static string SetName(object JSValue)
+    {
+        DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(Task[]));
+       // string lstItems = new JavaScriptSerializer().Deserialize<Task[]>(JSValue);
+        return "";
+    }
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+    }
+}
